97b18bcf6a4d82bac05e8a40664f7e481a8e8e30
PHAMTIENHUNG772006 <hungpham772006@gmail.com>
2024-11-26 19:25:04 +0700

    Add files via upload

---
+++ sesion6_bt7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	printf("moi ban nhap n");
+int number;
+int i;
+scanf("%d", &number);
+for(i=1;i<=n;i++){
+	if(number%i == 0){
+		printf("uoc cua so la %d",i);
+	}
+}
+}
